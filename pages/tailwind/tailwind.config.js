8f9c6f273fa266f51b83feff7c3f221f6ada0b0e
rhymaado <mightydhiyamatra@yahoo.co.id>
2023-10-05 14:00:45 +0700

    tailwind with external css and config

---
+++ pages/tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      padding: {
+        2.6: "10.250px",
+      },
+      colors: {
+        header: "#0b0909",
+        primary: "#ff8906",
+      },
+    },
+  },
+};
